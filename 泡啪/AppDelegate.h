56f8f67fbbb5ac58e096500ee3216bfedcd2ac26
liuyujing <liuyujingmusic@163.com>
2015-09-23 14:08:10 +0800

    Initial Commit

---
+++ 泡啪/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  泡啪
+//
+//  Created by Bruce on 15/9/23.
+//  Copyright (c) 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 泡啪/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  泡啪
+//
+//  Created by Bruce on 15/9/23.
+//  Copyright (c) 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
